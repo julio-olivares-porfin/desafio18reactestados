6cebbf04de4b21972df5a611bc19f49bdd05a6ff
julio-olivares-porfin <julio@por-fin.cl>
2024-06-01 03:48:38 -0400

    creación proyecto vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Registro from './components/Registro';
+import './App.css'
+
+
+function App() {
+
+  return (
+    <div className='main'>
+     <Registro />
+    </div>
+
+  )
+}
+
+export default App
